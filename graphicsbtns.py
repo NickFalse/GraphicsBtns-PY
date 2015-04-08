516c91dc3d671f854938926964d2b3a4f7520d5d
NickFalse <n.true@kingschristianacademy.org>
2015-04-08 14:28:46 -0400

    updated some of the functions nothing major

--- graphicsbtns.py
+++ graphicsbtns.py
@@ -1,23 +1,30 @@
 from graphics import *
 class button:
-    def __init__(self, name,x1,x2,y1,y2):
+    def __init__(self, win, name,x1,x2,y1,y2):
         self.name = name
         self.xmin = x1
         self.xmax = x2
         self.ymin = y1
         self.ymax = y2
+        self.wind = win
+        self.red = 0
+        self.green = 0
+        self.blue = 0
     def testBtn(self,p):
-        x = p.getX()
-        y = p.getY()
-        if (x>self.xmin)and(x<self.xmax)and(y>self.ymin)and(y<self.ymax):
-            return 1
-        else:
+        try:
+            x = p.getX()
+            y = p.getY()
+            if (x>self.xmin)and(x<self.xmax)and(y>self.ymin)and(y<self.ymax):
+                return 1
+            else:
+                return 0
+        except:
             return 0
     def setBtnColor(self, red, green, blue):
         self.red = red
         self.green = green
         self.blue = blue
-    def drawBtn(self, win):
+    def drawBtn(self):
         x1 = float(self.xmin)
         x2 = float(self.xmax)
         y1 = float(self.ymin)
@@ -28,13 +35,13 @@ class button:
         rec = Rectangle(p1, p2)
         rec.setFill(color)
         self.rect = rec
-        rec.draw(win)
+        rec.draw(self.wind)
         midx = (x2+x1)/2
         midy = (y2+y1)/2
         p3 = Point(midx, midy)
         title = Text(p3, self.name)
         self.title = title
-        title.draw(win)
+        title.draw(self.wind)
     def removeBtn(self):
         self.title.undraw()
         self.rect.undraw()
